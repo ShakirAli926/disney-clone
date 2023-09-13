27201463a79d5102d96126653c1fe0c494299316
ShakirAli926 <shakirbugti926@gmail.com>
2023-09-13 02:29:24 -0700

    route path is set

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css'
+import Login from "./Components/Login.jsx";
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
 
 function App() {
 
-  return (
-    <>
-        <h2>Hello</h2>
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login/>}/>
+                    <Route path="/about" element={<h2>About</h2>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App
