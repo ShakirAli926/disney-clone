a813037c1ffd6c82fee5ba85a5e0d3cebebaeb49
ShakirAli926 <shakirbugti926@gmail.com>
2023-09-16 06:07:54 -0700

    done logout functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
     Route
 } from "react-router-dom";
 import Header from "./Components/Header.jsx";
+import Home from "./Components/Home.jsx";
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
                 <Header />
                 <Routes>
                     <Route path="/" element={<Login/>}/>
+                    <Route path="/Home" element={<Home/>}/>
                 </Routes>
             </BrowserRouter>
         </>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,24 +7,42 @@ import {
     selectUserName,
     selectUserEmail,
     selectUserPhoto,
-    setUserLoginDetails
+    setUserLoginDetails, setSignOut
 } from '../features/user/userSlice.js'
+import {useEffect} from "react";
 
 const Header = (props) => {
     const dispatch = useDispatch();
     const history = useNavigate();
     const userName = useSelector(selectUserName);
-    const userEmail = useSelector(selectUserEmail);
     const userPhoto = useSelector(selectUserPhoto);
 
     const handleAuth = () => {
-        signInWithPopup(auth, provider).then(result => {
-            console.log(result)
-            setUser(result.user)
-        }).catch(err => {
-            console.log(err.message)
-        })
+        if(!userName){
+            signInWithPopup(auth, provider).then(result => {
+                console.log(result)
+                setUser(result.user)
+            }).catch(err => {
+                console.log(err.message)
+            })
+        } else if(userName){
+            auth.signOut().then(()=>{
+                dispatch(setSignOut());
+                history('/')
+            }).catch(err =>{
+                console.log(err.message)
+            })
+        }
     }
+
+    useEffect(()=>{
+        auth.onAuthStateChanged(async (user) =>{
+            if(user){
+                setUser(user)
+                history('/home')
+            }
+        })
+    },[userName])
 
     const setUser = (user) => {
         dispatch(setUserLoginDetails({
@@ -67,7 +85,12 @@ const Header = (props) => {
             </NavMenu>
             {
                 !userName ? <Login onClick={handleAuth}>Login</Login> : <>
-                    <UserImg src={userPhoto} alt={userName}></UserImg>
+                    <SignOut>
+                        <UserImg src={userPhoto} alt={userName}></UserImg>
+                        <DropDown>
+                            <span onClick={handleAuth}> Sign Out</span>
+                        </DropDown>
+                    </SignOut>
                 </>
             }
 
@@ -188,6 +211,43 @@ const Login = styled.a`
 
 const UserImg = styled.img`
   height: 100%;
+`
+const DropDown = styled.div`
+position: absolute;
+  top: 48px;
+  right : 0;
+  background: rgb(19,19,19);
+  border: 1px solid rgba(151,151,151,0.34);
+  border-radius : 4px;
+  box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+  padding : 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 110px;
+  opacity: 0;
+`
+
+const SignOut = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor : pointer;
+  align-items: center;
+  justify-content: center;
+  
+  ${UserImg}{
+    border-radius: 50px;
+    width: 100%;
+    height: 100%;
+  }
+  
+  &:hover {
+    ${DropDown}{
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
 `
 
 export default Header;
