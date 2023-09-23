38deed248f06e33c83857f75f5d87bddb7b24e67
ShakirAli926 <shakirbugti926@gmail.com>
2023-09-23 07:01:35 -0700

    added background on home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,10 +2,36 @@ import styled from 'styled-components';
 
 const Home = (props) => {
     return (
-        <div>
-            Home
-        </div>
+        <>
+         <Container>
+             <h2>Home</h2>
+             <h2>Home</h2>
+             <h2>Home</h2>
+             <h2>Home</h2>
+             <h2>Home</h2>
+             <h2>Home</h2>
+             <h2>Home</h2>
+         </Container>
+        </>
     );
 };
 
+const Container = styled.div`
+  position: relative;
+  min-height: cal(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.2vw + 5px);
+  
+  &:after  {
+    background: url("/images/home-background.png");
+    content: '';
+    position: absolute;
+    inset : 0px;
+    opacity: 1;
+    z-index: -1;
+    
+}
+`
 export default Home;
